6fdc17aeac91e4609a9ee672b9791b6bfb3a7db9
Yotam Kadishay <yotam.kadishay@gmail.com>
2019-04-11 13:59:30 +0300

    port def

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'ruby') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://place-hold.it/600X400"
+    self.thumb_image ||= "https://place-hold.it/350X200"
+  end
 end
